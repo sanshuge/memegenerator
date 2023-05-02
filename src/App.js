eadc82d3cd04a9665aaf4e58c575ec2ba4de89b3
Ruobing <chaiyichai791@gmail.com>
2023-05-02 11:25:26 -0600

    add react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Header from "./components/Header.js"
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+
+
+function Header(){
+    return (
+        <header className="header">
+        <img src="./images/Logo.png" />
+        <h2>Header component</h2>
+        <h4>React Course - Project 3</h4>
+    </header>
+        
+    )
+}
+
+export default Header;
